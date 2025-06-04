cbb78d7df67d8b8155dd00ca05cb81358f2c4684
강현호 <hhwj2270@gmail.com>
2025-06-04 11:32:28 +0900

    Feat: 1074 문제 풀이 중

---
+++ Acmicpc/src/divide/G5_Z_1074.java
@@ -0,0 +1,39 @@
+package divide;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class G5_Z_1074 {
+
+    public static void main(String[] args) {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int startX = 0;
+        int startY = 0;
+
+        // 10억
+
+        /*
+        N = 1
+        0, 0  | 1, 1
+        1, 1 | 2, 2
+
+        N = 2
+        0, 0 | 3, 3
+        1, 1 | 4, 4
+
+        N = 3
+        0, 0 | 7, 7
+        1, 1 | 8, 8
+
+        N = 4
+        0, 0 | 15, 15
+        1, 1 | 16, 16
+
+        몇 사분면인지 파고 들어가라
+
+         */
+
+    }
+}
